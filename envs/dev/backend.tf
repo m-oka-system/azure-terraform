1fe52870c7c5f69ca84897df1e03fde845d73f37
m-oka-system <m-oka-system@outlook.com>
2024-06-01 19:57:42 +0900

    backendのストレージアカウントを更新

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "azurermtfstate"
+    storage_account_name = "terraformstate8450"
     container_name       = "terraform-state"
     key                  = "dev.terraform.tfstate"
   }
