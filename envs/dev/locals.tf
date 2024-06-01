a8e80052156475d24fca4a5520778ddf2f19b095
m-oka-system <m-oka-system@outlook.com>
2024-06-01 19:58:54 +0900

    [update]ローカル変数に配列化した allowed_cidr を追加

--- envs/dev/locals.tf
+++ envs/dev/locals.tf
@@ -7,6 +7,7 @@ locals {
     subscription_id   = data.azurerm_subscription.primary.subscription_id
     tenant_id         = data.azurerm_subscription.primary.tenant_id
     random            = random_integer.num.result
+    allowed_cidr      = split(",", var.allowed_cidr)
     client_ip_address = chomp(data.http.ipify.response_body)
   }
 
